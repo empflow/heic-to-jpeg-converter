0f35695ef54032aa0a97f7c9b226e1273392bbad
empflow <empflow@tutanota.com>
2024-01-20 15:23:42 +0300

    possibly improve performance

--- src/convertAndSaveImgs.ts
+++ src/convertAndSaveImgs.ts
@@ -7,21 +7,28 @@ import getOutputImgPath from "./getOutputImgPath";
 
 export default async function convertAndSaveImgs() {
   const initImgsFilenames = await readdir(INPUT_IMGS_PATH);
+  const promises = [];
 
   for (let i = 0, n = initImgsFilenames.length; i < n; i++) {
     const inputImgPath = path.join(INPUT_IMGS_PATH, initImgsFilenames[i]);
-    if (path.extname(inputImgPath) !== ".heic") continue;
-
-    const inputImgBuffer = await readFile(inputImgPath);
-
-    const outputBuffer = await heicConvert({
-      buffer: inputImgBuffer,
-      format: "JPEG",
-      quality: 1,
-    });
-
-    const compressedOutputBuffer = await compressJpeg(outputBuffer);
-    const outputImgPath = getOutputImgPath(inputImgPath);
-    await writeFile(outputImgPath, compressedOutputBuffer);
+    promises.push(convertImg(inputImgPath));
   }
+
+  await Promise.all(promises);
+}
+
+async function convertImg(inputImgPath: string) {
+  if (path.extname(inputImgPath) !== ".heic") return;
+
+  const inputImgBuffer = await readFile(inputImgPath);
+
+  const outputBuffer = await heicConvert({
+    buffer: inputImgBuffer,
+    format: "JPEG",
+    quality: 1,
+  });
+
+  const compressedOutputBuffer = await compressJpeg(outputBuffer);
+  const outputImgPath = getOutputImgPath(inputImgPath);
+  await writeFile(outputImgPath, compressedOutputBuffer);
 }
